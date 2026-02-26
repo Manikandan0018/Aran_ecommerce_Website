dd1e3f413c84217e8bd6cf5b3eda4181dabd45c8
manikandan <manikandan110305@gmail.com>
2026-02-27 01:18:38 +0530

    update: improved Login UI/UX and removed bg image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { useEffect, useState } from "react";
 import API from "./services/api";
@@ -140,7 +141,8 @@ function App() {
       </Routes>
 
       <Analytics />
-      
+      <SpeedInsights/>
+
     </>
   );
 }
